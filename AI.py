b7696f7992ae5d414f7d8138b68ae966b0a781d4
jacobsm84 <jacobsm84@gmail.com>
2020-04-30 18:03:32 -0400

    Added gitignore and made the AI able to read through all the CSV files

--- AI.py
+++ AI.py
@@ -1,6 +1,19 @@
 import os, csv
 
 class AI:
-
+    
     def __init__(self):
         super().__init__()
+        self.get_data()
+    
+    def get_data(self):
+        years = ['2010','2011','2012','2013','2014','2015','2016','2017','2010','2018','2019']
+        for year in years:
+            input_file = ('{cwd}\stats\Hitters_{input_year}.csv'.format(cwd=os.getcwd(), input_year = year))
+            with open(input_file) as csvfile:
+                csv_reader = csv.reader(csvfile, delimiter = ',')
+                entry_count = 0
+                for row in csv_reader:
+                    entry_count += 1
+                print('THE NUMBER OF ENTRIES FOR {year} IS {count}'.format(year=year, count=entry_count))
+                    
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from AI import AI
 def main():
-    print("Hello World")
+    print("Welcome to the Baseball AI")
+    my_AI = AI()
+    
 
 if __name__ == "__main__":
     main()
